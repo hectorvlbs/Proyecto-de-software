4d03fc28e72e83f7f197754d737b05d831972a9d
Jesus Villalobos <jesusvillalobos2807@outlook.es>
2020-03-30 02:33:48 -0700

    Registrar editoriales

---
+++ ProyectoSW/src/Vistas/MainPruebas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vistas;
+
+/**
+ *
+ * @author villalobos28
+ */
+public class MainPruebas {
+
+    public static void main(String[] args) {
+        RegisterEditorial v = new RegisterEditorial();
+        v.setVisible(true);
+    }
+}
