de418804ea9d71c991d00b0df7898c04c8a5fd1c
jesusgil01 <39202049+jesusgil01@users.noreply.github.com>
2020-04-03 19:56:46 -0700

    Agrega consultas Gil

--- Consultas/Querys.java
+++ Consultas/Querys.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,8 +10,6 @@ package Consultas;
  * @author villalobos28
  */
 //  En está clase se insertan todas las consultas que el sistema va a ejecutar
-
-
 public class Querys {
 
     /**
@@ -27,9 +25,10 @@ public class Querys {
     public String RegisterEditorial() {
         return "{CALL RegisterEditorial(?,?,?,?,?)}";
     }
-    
+
     /**
      * Query
+     *
      * @ConsusltaUsuario
      * @author ArodyMG
      * @01/04/2020
@@ -39,6 +38,32 @@ public class Querys {
     public String ConsultaUsuario(String user, String pass) {
         return "SELECT rol  "
                 + " FROM bd85in3u2jlgurs2idgr.Empleados "
-                + " WHERE username = '"+user+"' AND password = '"+pass+"';";
+                + " WHERE username = '" + user + "' AND password = '" + pass + "';";
+    }
+
+    /**
+     * Query
+     *
+     * @InsertVenta
+     * @author Jesús Gil
+     * @ Marzo del 2020
+     * @1.0
+     * @Descripción: Este query Inserta una venta.
+     */
+    public String InsertVenta(){
+        return "{CALL InsertVenta(?,?)}";
+    }
+    
+        /**
+     * Query
+     *
+     * @InsertVentaLibrp
+     * @author Jesús Gil
+     * @ Marzo del 2020
+     * @1.0
+     * @Descripción: Este query Inserta en tabla venta_libro.
+     */
+    public String InsertVentaLibro(){
+        return "{CALL InsertVentaLibro(?,?)}";
     }
 }
